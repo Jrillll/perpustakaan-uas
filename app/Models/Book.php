5812de4b4db93cd05ca65f5756fd834b83600ffc
Nazriel <nazrielnararya1705@gmail.com>
2026-05-27 21:25:43 +0700

    init project

---
+++ app/Models/Book.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'author',
+        'publisher',
+        'publication_year',
+        'isbn',
+        'description',
+        'cover',
+        'stock',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function borrowings()
+    {
+        return $this->hasMany(Borrowing::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
